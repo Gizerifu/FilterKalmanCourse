1fcab0ec878e230f13e277ce40b1f3b8359a4274
Gizerifu <j.leguizamo.r@gmail.com>
2024-02-11 14:42:40 -0600

    Archivo actualizado con el script generativo comentado Todo se realiza en el for principal del experimento

--- Homework Sesson 3/Cart_LKF_FSF/Cart_Kalman_Filter.m
+++ Homework Sesson 3/Cart_LKF_FSF/Cart_Kalman_Filter.m
@@ -6,12 +6,19 @@ clearvars;
 close all;
 
 b=1; m=2; k_m=5; Ts=1e-3; L=30;
-Cart_generative_model;
+%Cart_generative_model;
 
-subplot(2,1,1)
-plot(t,x(1,:),t,x(2,:),t,y)
-legend('x1 generative','x2 generative','y generative')
+A=[[0,1];[-k_m/m,-b/m]];
+B=[0,1/m]';
+C=[1,0];
+D=0;
 
+Ad=eye(2)+Ts*A;
+Bd=Ts*B;
+t=0:Ts:L;
+x(:,1)=[6,3]';
+w=5e-6;%3e-5;
+v=1e-3;
 Q=[w,w]*[w,w]';
 R=v^2;
 P=1e4*eye(2);
@@ -28,13 +35,9 @@ k_c = [5 1];
 xDeseada = [50 0];
 x_c = zeros([2,length(t)]);
 y_c = zeros([2,length(t)]);
-%x(:,1)=[2,3];
 
 for k=2:length(t)
 
-    %x(:,k)=Ad*x(:,k-1)+Bd*u_c(k-1)+w*randn([2,1]);
-    %y(k)=C*x(:,k)+v*randn;
-    
     u_c(k) = k_c(1)*(xDeseada(1)-x_est(1,k)) + k_c(2)*(xDeseada(2)-x_est(2,k)) ;
     x_c(:,k)=Ad*x_c(:,k-1)+Bd*u_c(k-1)+w*randn([2,1]);
     y_c(k)=C*x_c(:,k)+v*randn;
@@ -49,25 +52,19 @@ for k=2:length(t)
     L=P_xy/P_y;
     x_est(:,k)=x_pred(:,k)+L*(y_c(k)-y_pred(k));
     P=P_pred-L*P_y*L';
-  
     
-    %u_c(k) = (k_c(1)*(xDedesada(1)-y_pred(k)) + k_c(2)*(xDedesada(2)-y_pred(k)));
-     if k<100
-     Q=(1-alpha)*Q+alpha*L*(y(k)-y_pred(k))*(y(k)-y_pred(k))'*L';
-     end
+    % if k<100
+    % Q=(1-alpha)*Q+alpha*L*(y_c(k)-y_pred(k))*(y_c(k)-y_pred(k))'*L';
+    % end
 end
 
-subplot(2,1,2)
-plot(t,x_c(1,:),t,x_c(2,:),t,y_c)
-legend('x1 update','x2 update', 'y update')
-figure
 subplot(3,1,1)
-plot(t,x_c(1,:))
-%hold on
-%plot(t,y_c)
+plot(t,x_c(1,:),'r')
 hold on
-plot(t,x_est(1,:))
-legend('x1 ', 'x1 estimated')
+plot(t,y_c,'k')
+hold on
+plot(t,x_est(1,:),'y')
+legend('x1 ', 'y','x1 estimated')
 subplot(3,1,2)
 plot(t,x_c(2,:))
 hold on
